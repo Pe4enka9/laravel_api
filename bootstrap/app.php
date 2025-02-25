22e6d782bd831f080fa5760ad6e964590121634b
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-02-25 21:27:56 +0700

    last

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,16 +7,26 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->renderable(function (AuthenticationException $exception, $request) {
-            return response()->json([], 401);
+//        $exceptions->render(function (AuthenticationException $exception, Request $request) {
+//            if ($request->is('api/*')) {
+//                return response()->json([], 401);
+//            }
+//        });
+
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            if ($request->is('api/*')) {
+                return response()->json([], 401);
+            }
+
+            return $request->expectsJson();
         });
     })->create();
